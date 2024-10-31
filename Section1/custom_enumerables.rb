3bbffb056216a7ca45bb0adb6822015af034b963
öykü parlak <56317041+oykuparlakk@users.noreply.github.com>
2024-10-31 16:38:42 +0300

    recursive fibonacci algorithm added

---
+++ Section1/custom_enumerables.rb
@@ -0,0 +1,5 @@
+
+a = [1,2,3,4]
+
+puts a.find { |n| n == 2}
+puts a.find { |n| n == 10}
---
+++ Section1/recursion.rb
@@ -0,0 +1,16 @@
+puts "Please enter the number"
+fib_recursion_number = gets.chomp.to_i
+@fib_arr = [0, 1]
+
+def fibs_rec(rec_number)
+  return @fib_arr[0...rec_number] if rec_number <= 2
+
+  if @fib_arr.size < rec_number
+    @fib_arr << @fib_arr[-1] + @fib_arr[-2]
+    fibs_rec(rec_number) 
+  else
+    @fib_arr[0...rec_number] 
+  end
+end
+
+puts fibs_rec(fib_recursion_number).join(", ")
